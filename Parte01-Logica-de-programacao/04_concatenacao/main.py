94ee016dd1e863a51519f9a81197d297f7f1c4a1
Ségio Artifon <artifonn@gmail.com>
2026-06-26 20:15:51 -0300

    feat: implementa atividade 04 concatenacao

--- Parte01-Logica-de-programacao/04_concatenacao/main.py
+++ Parte01-Logica-de-programacao/04_concatenacao/main.py
@@ -1,6 +1,8 @@
 nome = input("Informe seu nome: ")
+telefone = input('Informe um telefone para contato: ')
 
-print("Olá,", nome,".")
-print('Olá ' + nome + '.')
-print('Olá {}.'.format(nome))
-print(f'Olá, {nome}.')
+
+print("Olá, ", nome,", o número de telefone ", telefone, ' foi salvo com sucesso.')
+print('Olá ' + nome + ', o número de telefone' + telefone + ' foi salvo com sucesso.')
+print('Olá {}, o número de telefone {}.'.format(nome, telefone))
+print(f'Olá, {nome}, o número de telefone {telefone} foi salvo com sucesso.')
