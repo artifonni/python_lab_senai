2edadc8bb6c6713a5512001f7f5feea26586d867
Sérgio <artifonni@gmail.com>
2026-07-23 21:21:16 -0300

    feat: adiciona tratamento de exceções

--- Parte01-Logica-de-programacao/17_arquivos/arquivos/main.py
+++ Parte01-Logica-de-programacao/17_arquivos/arquivos/main.py
@@ -2,32 +2,34 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-while True:
-    print('1 - Gravar arquivos | 2 - Ler arquivo | 3 - Encerrar programa')
+try:
+    while True:
+        print('1 - Gravar arquivos | 2 - Ler arquivo | 3 - Encerrar programa')
 
-    opcao = input('Iforme a opção desejada: ').strip()
-    os.system('cls' if os.name == 'nt' else 'clear')
+        opcao = input('Iforme a opção desejada: ').strip()
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    match opcao:
-        case '1':
-            novo_texto = input('Digite o seu texto: ')
-            nome_arquivo = input('Informe o nome do arquivo: ').strip()
-            with open(f"17_arquivos/arquivos/{nome_arquivo}.txt", "w", encoding="utf-8") as f:
-                f.write(novo_texto)
-        case '2':
-            nome_arquivo = input('Informe o nome do arquivo: ').strip()
-            try: 
-                with open(f"17_arquivos/arquivos/{nome_arquivo}.txt", "r", encoding="utf-8") as f:
-                                conteudo = f.read()
-                                print(conteudo)
-            except FileNotFoundError as e:
-                print(f'Arquivo não encontrado! {e}')
+        match opcao:
+            case '1':
+                novo_texto = input('Digite o seu texto: ')
+                nome_arquivo = input('Informe o nome do arquivo: ').strip()
+                with open(f"17_arquivos/arquivos/{nome_arquivo}.txt", "w", encoding="utf-8") as f:
+                    f.write(novo_texto)
+            case '2':
+                nome_arquivo = input('Informe o nome do arquivo: ').strip()
+                try: 
+                    with open(f"17_arquivos/arquivos/{nome_arquivo}.txt", "r", encoding="utf-8") as f:
+                                    conteudo = f.read()
+                                    print(conteudo)
+                except FileNotFoundError as e:
+                    print(f'Arquivo não encontrado! {e}')
+                    continue
+            case '3':
+                print('Encerrando...', end='\n')
+                print('Programa finalizado.')
+                break
+            case _:
+                print('Opção inválida.')
                 continue
-        case '3':
-            print('Encerrando...', end='\n')
-            print('Programa finalizado.')
-            break
-        case _:
-            print('Opção inválida.')
-            continue
-
+except Exception as error:
+     print(f'Erro inesperado', {error})
