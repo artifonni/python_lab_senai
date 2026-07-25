3b6ab81e373c94dd89525bfd8b53861f72b3d72e
Sérgio Artifon <artifonni@gmail.com>
2026-07-24 21:26:25 -0300

    feat: implementa atividade 01

---
+++ Atividades/lista_atividades_01/README.md
@@ -0,0 +1,3 @@
+# Atividade 01
+
+Crie um programa que receva o nome, peso e altura do usuário e informe o seu diagnostico com base no IMC.
---
+++ Atividades/lista_atividades_01/programa_01-01/main.py
@@ -0,0 +1,48 @@
+from bisect import bisect
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+try:
+    nome = input('Informe o seu nome: ').strip()
+    altura = float(input('Informe a sua altura em cm: ').replace(',','.').strip())
+    peso = float(input('Informe o seu peso: ').replace(',','.'))
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    while True:
+        print('1 - Calcuar IMC | 2 - Encerrar o programa')
+
+        opcao = int(input('Infome a opção deseada: ').strip())
+
+        match opcao:
+            case 1:
+                imc = peso / (altura * altura)
+
+                limites = [18.5, 25.0, 30.0, 35.0, 40.0]
+
+                categorias = [
+                    'Magreza (Baixo peso)',
+                    'Peso normal (Peso adequado)',
+                    'Sobrepeso (Pré-obesidade)',
+                    'Obesidade Grau I',
+                    'Obesidade Grau II',
+                    'Obesidade Grave (Grau III)'
+                ]
+
+                indice = bisect(limites, imc)
+                # print(f'IMC: {imc:.2f}')
+                resultado = f'[\nNome: {nome}, \nAltura: {altura},\nPeso: {peso}, \nResultado: {imc:.2f} - {categorias[indice]}\n]'
+
+                meu_arquivo = 'Resultado - IMC'
+                with open(f"programa_01-01/{meu_arquivo}.txt", "w", encoding="utf-8") as f: f.write(resultado)
+                
+            case 2:
+                print('Encerrando...', end='\nPrograma encerrado!')
+                break
+            case _:
+                print('Opção inválida!')
+                continue
+        
+except Exception as error:
+    print(f'Erro: {error}')
